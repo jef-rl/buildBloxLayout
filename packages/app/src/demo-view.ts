ce92d8ae9286ed6f8d53ce0766c4adaa5069d9b2
jef-3n <jef@gourmetguide.co.uk>
2026-01-16 16:33:36 +0000

    Use context consumers for app state

--- packages/app/src/demo-view.ts
+++ packages/app/src/demo-view.ts
@@ -1,21 +1,17 @@
 import { LitElement, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-import { uiState } from '@project/framework';
+import { ContextConsumer } from '@lit/context';
+import { customElement } from 'lit/decorators.js';
+import { uiStateContext } from '@project/framework';
 
 @customElement('demo-view')
 export class DemoView extends LitElement {
-  @property({ type: Object }) state = uiState.getState();
-
-  constructor() {
-    super();
-    uiState.subscribe(this.handleStateUpdate.bind(this));
-  }
-
-  handleStateUpdate(newState) {
-    this.state = newState;
-  }
+  private uiStateConsumer = new ContextConsumer(this, {
+    context: uiStateContext,
+    subscribe: true,
+  });
 
   render() {
-    return html`<div>${this.state.message}</div>`;
+    const message = this.uiStateConsumer.value?.state?.message;
+    return html`<div>${message}</div>`;
   }
 }
