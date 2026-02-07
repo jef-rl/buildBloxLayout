8fd6be89ee97d90aed5de108e6e3a9844fbd0bf4
jef <jef@gourmetguide.co.uk>
2026-02-07 21:06:27 +0000

    remove legacy

--- packages/framework/nxt/compat/ui-state-context-value.ts
+++
@@ -1,4 +0,0 @@
-import type { CoreContext } from '../runtime/context/core-context';
-import type { FrameworkState } from '../runtime/state/framework-state';
-
-export type UiStateContextValue = CoreContext<FrameworkState>;
